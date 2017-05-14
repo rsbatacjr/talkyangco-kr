6886fb03ae5ab9531a401d4047c41b8060866ae5
rey <reybatacjr@gmail.com>
2017-05-14 11:30:38 +0800

    Initial

---
+++ footer.php
@@ -0,0 +1,135 @@
+			<div class="row">
+				<footer>
+					<div id="footer-menu">
+						<div class="container">
+							<div class="row">
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">Introduction</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">ABOUT US</a></li>
+										<li><a href="">Greetings</a></li>
+										<li><a href="">Strengths</a></li>
+										<li><a href="">Location</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">Process</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">Procedure</a></li>
+										<li><a href="">About preparation</a></li>
+										<li><a href="">FAQ</a></li>
+										<li><a href="">Medical &amp; Insurance</a></li>
+										<li><a href="">The way to Baguio</a></li>
+										<li><a href="">Campaign</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">Registration</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">Tuition Fee</a></li>
+										<li><a href="">Dormitory types</a></li>
+										<li><a href="">Miscellaneous fees</a></li>
+										<li><a href="">IELTS  Official test dates</a></li>
+										<li><a href="">TOEIC Official test dates</a></li>
+										<li><a href="">Holiday in the Philippines</a></li>
+										<li><a href="">Official pick-up dates</a></li>
+										<li><a href="">Refund policy</a></li>
+										<li><a href="">E-Application(Register now)</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">Talk’s Know-how</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">ESL 421, 521, 611</a></li>
+										<li><a href="">Intensive IELTS</a></li>
+										<li><a href="">Intensive TOEIC</a></li>
+										<li><a href="">IELTS Guarantee</a></li>
+										<li><a href="">TOEIC Guarantee</a></li>
+										<li><a href="">Power Speaking</a></li>
+										<li><a href="">Working Holiday</a></li>
+										<li><a href="">Family ESL</a></li>
+										<li><a href="">Online Consulting</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">Studying Plans</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">TOEIC test</a></li>
+										<li><a href="">IELTS test</a></li>
+										<li><a href="">Power speaking</a></li>
+										<li><a href="">Short-term(1~3wks study)</a></li>
+										<li><a href="">Long-term study</a></li>
+										<li><a href="">Best price</a></li>
+										<li><a href="">Philippines &amp; Other country</a></li>
+									</ul>
+								</div>
+								<div class="col-md-2">
+									<ul>
+										<li><a href="">Community</a></li>
+										<li>&nbsp;</li>
+										<li><a href="">Notice</a></li>
+										<li><a href="">Student’s experience</a></li>
+										<li><a href="">Talk’s meals</a></li>
+										<li><a href="">Inside the Philippines</a></li>
+										<li><a href="">Best Student &amp; Teacher</a></li>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="container">
+						<div class="row" style="margin-top: 30px; margin-bottom: 30px;">
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/bureau-immigration.png" style="width: 90px; height: 90px;">
+								<p>SSP<br>(Special Study Permit)</p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/tesda.png" style="width: 90px; height: 90px;">
+								<p>TESDA</p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/pcc.png" style="width: 90px; height: 90px;">
+								<p>Accreditation<br>certificate of TVET </p>
+							</div>
+							<div class="footer-icons col-xs-6 col-sm-3 text-center">
+								<img src="<?php echo THEME_URI ?>/img/besa.png" style="width: 90px; height: 90px;">
+								<p>Baguio English<br>Schools Association</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-6">
+								<p>
+									Yangco Center<br>
+									Tel : +63-74-442-6012<br>
+									주소 : #37 yangco Rd., Upper General Luna Rd., Baguio City, 2600 Philippines<br>
+									President Kenneth : +63-917-568-9161
+								</p>
+							</div>
+							<div class="col-md-6">
+								<p>
+									EnE Center<br>
+									Tel : +63-74-442-6012<br>
+									주소 : Lot A, Dacanay Building, Marcos Highway, Baguio City, 2600 Philippines<br>
+									President Louis : +63-917-688-8656
+								</p>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-md-12 text-center">
+								<p><br/><br/>
+								<a href="mailto:baguiotalk@gmail.com">baguiotalk@gmail.com</a> &copy; <i>Canitalk Academy Inc. All rights reserved.</i></p>
+							</div>
+						</div>
+					</div> 
+				</footer>
+			</div>
+		</div>
+	</body>
+	<script type="text/javascript" src="<?php echo THEME_URI ?>/js/jquery.min.js"></script>
+	<script type="text/javascript" src="<?php echo THEME_URI ?>/js/bootstrap.min.js"></script>
+</html>
---
+++ home.php
@@ -0,0 +1,268 @@
+<?php
+get_header();
+?>
+<div class="container">
+	<div class="row">
+		<div id="qualified-programs-container" class="col-xs-12 col-md-8">
+			<div id="qualified-programs">
+				<div class="mask">
+					<h1>토크 어학 연수 과정</h1>
+				<p style="max-width: 400px;">자유롭게 의사소통을 할 수 있는 일반 회화과정부터 취업 준비 및 유학, 이민을 위한 시험 대비 과정까지  다양하게 준비되어 있습니다.</p>
+				<div id="course-builder-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<strong>&nbsp;나에게 맞는 코스 둘러보기 :</strong>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>회화과정</strong>
+								<p>ESL 521</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>취업준비</strong>
+								<p>Intensive TOEIC</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>유학, 이민</strong>
+								<p>Intensive IELTS</p>
+							</div>
+						</div>
+						<div class="col-xs-12 col-sm-6 col-md-3">
+							<div class="course-builder">
+								<strong>가족연수</strong>
+								<p>Family ESL</p>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+				</div>
+			</div>
+		</div>
+		<div id="about-container" class="col-xs-12 col-sm-6 col-md-4">
+			<div id="about">
+				<div class="mask">
+					<h2>학원 둘러보기</h2>
+					<div id="about-normal">
+						<p>입학 전 미리 보는 기숙사와 강의실</p>
+						<span class="plus-sign">+</span>
+						<div class="clearfix"></div>
+					</div>
+					<div id="about-hover">
+						<p>
+						입학적엔 토크의 기숙사, 강의실, 부대시설 등을 3D로 보실 수 있습니다. 둘러보신 후에 연수준비물도 함께 보시면 더욱 편안한 기숙사 생활을 하실 수 있습니다.
+						</p>
+						<span style="position: absolute;bottom: 10px;right: 10px;display: inline-block;margin-bottom: 0;" class="next-button"></span>
+						<div class="clearfix"></div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+		<div id="experience-container" class="col-xs-12 col-sm-6 col-md-4">
+			<div id="experience">
+				<div class="mask">
+					<h2>연수후기</h2>
+					<div id="experience-normal">
+						<p>학원에 일상생활 미리보기</p>
+						<span class="plus-sign">+</span>
+					</div>
+					<div id="experience-hover">
+						<p>
+							현재 공부하고 있는 학생들의 생활과 졸업생들의 후기를 보시고 연수오실 때 어떻게 하면 더욱 효과적인 어학연수를 할 수 있을지 생각해보실 수 있습니다.
+						</p>
+						<span style="position: absolute;bottom: 10px;right: 10px;display: inline-block;margin-bottom: 0;" class="next-button"></span>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="notice-container" class="col-xs-12 col-md-4">
+			<div id="notice">
+				<strong>공지사항</strong>
+				<p>
+					토크의 최근 소식들을 미리 보실 수 있으며, 출국 전에 미리미리 확인하여 알찬 어학연수를 준비하시기 바랍니다.
+				</p>
+				<div id="notice-list-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<span id="notice-plus">+</span>
+							<span class="next-button"></span>
+						</div>
+					</div>
+					<ul id="notice-list">
+						<li><a href="#">5월 입학가능일 안내</a></li>
+						<li><a href="#">노용이 화난 날(Warning)</a></li>
+						<li><a href="#">노용이 소리 질른 날..ㅋㅋ</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="plans-container" class="col-xs-12 col-md-8">
+			<div id="plans">
+				<div class="mask">
+					<strong>필리핀어학연수 계획</strong>
+					<div id="plan-list-container" class="row">
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>IELTS<br>5.0, 5.5, 6.0</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>토크에서 보장하는 IELTS 점수로 해외대학에 입학 및 이민을 준비해 보세요.</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>스피킹<br>집중과정</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>문법과 단어는 잘 알지만 회화가 부족하다면 스피킹 집중과정으로 마무리 해보세요.</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>1~3주<br>단기 어학연수</p>
+											<span class="plus-sign">+</span>
+										</div>	
+										<div class="plan-list-hover">
+											<p>시간이 부족하시면 영어에 대한 올바른 공부방법과 회화를 단기에 만들어보세요. </p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+						<div class="row">
+							<div class="col-xs-12">
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>TOEIC<br>600, 700, 800, 900</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>필리핀에서 최초의 보장반을 운영하는 토크에서 TOEIC점수와 TOEIC Speaking을 완벽하게 만들어 바로 취업하세요.</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>호주, 캐나다<br>연계연수</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>미국, 캐나다, 호주, 유럽으로 유학을 가기 전에 아직 영어가 서투르다면 토크에서 탄탄하게 기본을 만들어 가세요.</p>
+											<span class="next-button-black"></span>
+										</div>
+										
+									</div>
+								</div>
+								<div class="plan-list-parent col-xs-12 col-md-4">
+									<div class="plan-list">
+										<div class="plan-list-normal">
+											<p><br>저렴한<br>어학연수</p>
+											<span class="plus-sign">+</span>
+										</div>
+										<div class="plan-list-hover">
+											<p>어학연수 비용이 부담스러우신가요? 토크에 오시면 학생들을 위해 저렴한 어학연수를 준비해 놨습니다.</p>
+											<span class="next-button-black"></span>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="clearfix"></div>
+			</div>
+		</div>
+	</div>
+	<div class="row">
+		<div id="best-place-container" class="col-xs-12 col-md-4">
+			<div id="best-place">
+				<div class="mask">
+					<strong>여행 코스</strong>
+					<div id="best-place-list-container">
+						<ul id="best-place-list">
+							<li><a href="#">바기오(Baguio City)</a></li>
+							<li><a href="#">당일치기 여행 코스</a></li>
+							<li><a href="#">1박2일 또는 2박3일 여행코스</a></li>
+						</ul>
+					</div>
+				</div>
+			</div>
+		</div>
+		<div id="campaign-container" class="col-xs-12 col-md-4">
+			<div id="campaign">
+				<strong>진행중인 캠페인</strong>
+				<div id="campaign-list-container">
+					<ul id="campaign-list">
+						<li><a href="#">IELTS수업을 Native와 함께 하자</a></li>
+						<li><a href="#">12주에 TOEIC 300점 UP</a></li>
+						<li><a href="#">무료 TOEIC 공인시험 제공</a></li>
+					</ul>
+				</div>
+			</div>
+		</div>
+		<div id="calendar-container" class="col-xs-12 col-md-4">
+			<div id="calendar">
+				<strong>Calendar</strong>
+				<div id="calendar-list-container">
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="calendar-list-parent col-xs-6" style="border-right:1px solid #666; border-bottom: 1px solid #666">
+								<div class="calendar-list">
+									<p><br>필리핀 공휴일</p>
+								</div>
+							</div>
+							<div class="calendar-list-parent col-xs-6" style=" border-bottom: 1px solid #666">
+								<div class="calendar-list">
+									<p><br>단체픽업</p>
+								</div>
+							</div>
+						</div>
+					</div>
+					<div class="row">
+						<div class="col-xs-12">
+							<div class="calendar-list-parent col-xs-6" style="border-right:1px solid #666">
+								<div class="calendar-list">
+									<p><br>IELTS<br>공인시험</p>
+								</div>
+							</div>
+							<div class="calendar-list-parent col-xs-6">
+								<div class="calendar-list">
+									<p><br>TOEIC<br>공인시험</p>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+<div class="row">
+	<img src="<?php echo THEME_URI; ?>/img/sample-image1.png" class="img-responsive"/>
+</div>
+<?php 
+get_footer();
