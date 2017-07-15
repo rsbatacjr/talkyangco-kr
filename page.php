7ceb6ff5a52f2d278ceaebad77b1a8f988a60bc9
root <root@ubuntu-2gb-sgp1-01.localdomain>
2017-07-15 04:25:49 +0000

    Create new post type for travel.

--- page.php
+++ page.php
@@ -1,12 +1,12 @@
 <?php 
 	get_header();
 ?>
-	<div class="row">
+<div class="row">
 		<?php while (have_posts()) : the_post(); ?>
 	        <?php
 			the_content();
 			?>
 	<?php endwhile; ?>
-	</div>
+</div>
 <?php
 	get_footer();
