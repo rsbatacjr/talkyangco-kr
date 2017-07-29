c43faccd51676a3f9bd3b717bf8bfac751c75955
Reynaldo S. Batac Jr <reynaldobatac@Reynaldos-MacBook-Air.local>
2017-07-29 11:51:40 +0800

    Fixed footer, header and home html tags indentation.

--- header.php
+++ header.php
@@ -26,96 +26,96 @@
 
 </head>
 	<body>
-<div id="floating-search-tb" class="hidden">
-<div class="input-group">
-    <input id="s" type="text" class="form-control"/>
-    <span class="input-group-addon">
-        <i class="fa fa-search"></i>
-    </span>
-</div>
-</div>
-<div id="floating-menu" class="hidden">
-          <div class="container">
-<div class="col-xs-12 col-sm-12 col-md-8">
-                    <ul class="nav navbar-nav">
-                      <li><a href="http://128.199.136.69:81/about-us/">학교소개</a></li>
-                      <li><a href="http://128.199.136.69:81/procedure/">유학까지의 흐름</a></li>
-                      <li><a href="#">연수비용</a></li>
-                      <li><a href="#">토크의 학습방법</a></li>
-                      <li><a href="#">필리핀어학연수계획</a></li>
-                      <li><a href="#">커뮤니티</a></li>
-                    </ul>
+    <div id="floating-search-tb" class="hidden">
+      <div class="input-group">
+          <input id="s" type="text" class="form-control"/>
+          <span class="input-group-addon">
+              <i class="fa fa-search"></i>
+          </span>
+      </div>
+    </div>
+    <div id="floating-menu" class="hidden">
+      <div class="container">
+              <div class="col-xs-12 col-sm-12 col-md-8">
+                <ul class="nav navbar-nav">
+                  <li><a href="http://128.199.136.69:81/about-us/">학교소개</a></li>
+                  <li><a href="http://128.199.136.69:81/procedure/">유학까지의 흐름</a></li>
+                  <li><a href="#">연수비용</a></li>
+                  <li><a href="#">토크의 학습방법</a></li>
+                  <li><a href="#">필리핀어학연수계획</a></li>
+                  <li><a href="#">커뮤니티</a></li>
+                </ul>
+              </div>
+              <div class="col-xs-12 col-sm-12 col-md-4">
+                <div class="row">
+                  <div class="search-lens">
+                    <span class="fa fa-search"></span>
+                    <!-- <input placeholder="수색"> -->
                   </div>
-                  <div class="col-xs-12 col-sm-12 col-md-4">
-                    <div class="row">
-<div class="search-lens">
-  <span class="fa fa-search"></span>
-  <!-- <input placeholder="수색"> -->
-</div>
-                    </div>
-                  </div>
-
-          </div>
-        </div>
-
+                </div>
+              </div>
+      </div>
+    </div>
 		<div class="container-fluid">
-<div class="row">      <header>
-         <div id="weblinks">
-            <div class="container">
-              <a href="" style="border: 1px solid #fff; padding: 3px 5px; font-size: 12px; border-radius: 3px">AGENCY</a>&nbsp;&nbsp;
-              <a href="#">ENG</a>
-              <a href="#">JPN</a>
-              <a href="#">TW</a>
-              <a href="#">CN</a>
-              <a href="#">VN</a>
+      <div class="row">
+        <header>
+          <div id="weblinks">
+              <div class="container">
+                <a href="" style="border: 1px solid #fff; padding: 3px 5px; font-size: 12px; border-radius: 3px">AGENCY</a>&nbsp;&nbsp;
+                <a href="#">ENG</a>
+                <a href="#">JPN</a>
+                <a href="#">TW</a>
+                <a href="#">CN</a>
+                <a href="#">VN</a>
+              </div>
             </div>
-          </div>
-          <div class="container">
-              <nav class="navbar navbar-default" role="navigation">
-                <div class="navbar-header">
-                  <p class="visible-xs" style="position: absolute; top: 15px; right: 65px;"><a href="#">Sign in</a> / <a href="#">Join</a> | Menu</p>
-                  <div class="hidden-xs row" style="position: absolute; top: 16px;">
-                    <div class="container">
+            <div class="container">
+                <nav class="navbar navbar-default" role="navigation">
+                  <div class="navbar-header">
+                    <p class="visible-xs" style="position: absolute; top: 15px; right: 65px;"><a href="#">Sign in</a> / <a href="#">Join</a> | Menu</p>
+                    <div class="hidden-xs row" style="position: absolute; top: 16px;">
+                      <div class="container">
                       <div class="col-xs-12 col-md-8">
                         <div id="login-box">
                           <a href="#">Sign in</a> / <a href="#">Join</a>
                           <hr style="margin-top: 5px;">
                         </div>
                       </div>
-                    <div class="col-xs-12 col-md-4">
-                      <div id="quick-links">
-                        <a href="#">안내책자</a> | <a href="#">OT자료</a> | <a href="#">상담문의</a>
+                      <div class="col-xs-12 col-md-4">
+                        <div id="quick-links">
+                          <a href="#">안내책자</a> | <a href="#">OT자료</a> | <a href="#">상담문의</a>
+                        </div>
+                      </div>
                       </div>
                     </div>
-                    </div>
+                    <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-collapse">
+                      <span class="sr-only">Toggle navigation</span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                      <span class="icon-bar"></span>
+                    </button>
+                    <a id="logo" class="navbar-brand" href="<?php echo HOME_PAGE; ?>"><img src="<?php echo THEME_URI; ?>/img/talk-logo.png"></a>
                   </div>
-                  <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#example-navbar-collapse">
-                    <span class="sr-only">Toggle navigation</span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                    <span class="icon-bar"></span>
-                  </button>
-                  <a id="logo" class="navbar-brand" href="<?php echo HOME_PAGE; ?>"><img src="<?php echo THEME_URI; ?>/img/talk-logo.png"></a>
-                </div>
-                <div class="collapse navbar-collapse" id="example-navbar-collapse">
-                  <div class="col-xs-12 col-sm-12 col-md-8">
-                    <ul class="nav navbar-nav">
-                      <li><a href="http://128.199.136.69:81/about-us/">학교소개</a></li>
-                      <li><a href="http://128.199.136.69:81/procedure/">유학까지의 흐름</a></li>
-                      <li><a href="#">연수비용</a></li>
-                      <li><a href="#">토크의 학습방법</a></li>
-                      <li><a href="#">필리핀어학연수계획</a></li>
+                  <div class="collapse navbar-collapse" id="example-navbar-collapse">
+                    <div class="col-xs-12 col-sm-12 col-md-8">
+                      <ul class="nav navbar-nav">
+                        <li><a href="http://128.199.136.69:81/about-us/">학교소개</a></li>
+                        <li><a href="http://128.199.136.69:81/procedure/">유학까지의 흐름</a></li>
+                        <li><a href="#">연수비용</a></li>
+                        <li><a href="#">토크의 학습방법</a></li>
+                        <li><a href="#">필리핀어학연수계획</a></li>
                       <li><a href="#">커뮤니티</a></li>
-                    </ul>
-                  </div>
-		  <div class="col-xs-12 col-sm-12 col-md-4">
-                    <div class="row">
-<div class="search-lens">
-  <span class="fa fa-search"></span>
-</div>
+                      </ul>
+                    </div>
+                    <div class="col-xs-12 col-sm-12 col-md-4">
+                      <div class="row">
+                        <div class="search-lens">
+                          <span class="fa fa-search"></span>
+                        </div>
+                      </div>
                     </div>
                   </div>
-                </div>
-              </nav>
-          </div>
-      </header></div>
+                </nav>
+            </div>
+        </header>
+      </div>
