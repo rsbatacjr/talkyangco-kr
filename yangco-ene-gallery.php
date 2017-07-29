16718d1c0a8e1bff2ce3d470c57a733d82b8c064
Reynaldo S. Batac Jr <reynaldobatac@Reynaldos-MacBook-Air.local>
2017-07-29 15:02:16 +0800

    Update gallery template

--- yangco-ene-gallery.php
+++ yangco-ene-gallery.php
@@ -5,6 +5,16 @@
 	get_header("mobile");
 ?>
 <div class="container">
+	<div class="row">
+		<div class="col-xs-12">
+			<?= listGalleryImage('yangco-gallery'); ?>
+		</div>
+	</div>
+	<div class="row">
+		<div class="col-xs-12">
+			<?= listGalleryImage('ene-gallery'); ?>
+		</div>
+	</div>
 </div>
 <?php
 	get_footer("mobile");
