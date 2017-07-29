1db5f94f3b7ed455bc77612532d7e31d6271add3
Reynaldo S. Batac Jr <reynaldobatac@Reynaldos-MacBook-Air.local>
2017-07-29 13:34:25 +0800

    Update floating menu

--- header.php
+++ header.php
@@ -38,15 +38,75 @@
       <div class="container">
               <div class="col-xs-12 col-sm-12 col-md-8">
                 <ul class="nav navbar-nav">
-                  <li class="dropdown-menu">
-			<a href="http://128.199.136.69:81/about-us/">학교소개</a>
-			
-		  </li>
-                  <li><a href="http://128.199.136.69:81/procedure/">유학까지의 흐름</a></li>
-                  <li><a href="#">연수비용</a></li>
-                  <li><a href="#">토크의 학습방법</a></li>
-                  <li><a href="#">필리핀어학연수계획</a></li>
-                  <li><a href="#">커뮤니티</a></li>
+                  <li class="dropdown-submenu">
+                    <a href="#">학교소개</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="http://128.199.136.69:81/about-us/">연혁</a></li>
+                      <li><a href="http://128.199.136.69:81/greetings/">인사말</a></li>
+                      <li><a href="http://128.199.136.69:81/strengths/">특징</a></li>
+                      <li><a href="http://128.199.136.69:81/location/">오시는 길</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown-submenu">
+                    <a href="#">유학까지의 흐름</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="http://128.199.136.69:81/procedure/">유학까지의 흐름</a></li>
+                      <li><a href="http://128.199.136.69:81/about-preparation/">유학준비에 대해</a></li>
+                      <li><a href="http://128.199.136.69:81/faqs/">FAQ</a></li>
+                      <li><a href="http://128.199.136.69:81/medical-and-insurance/">의료 체제와 보험</a></li>
+                      <li><a href="http://128.199.136.69:81/way-to-baguio/">바기오 가는 방법</a></li>
+                      <li><a href="http://128.199.136.69:81/campaigns/">캠페인</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown-submenu">
+                    <a href="#">연수비용</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="http://128.199.136.69:81/tuition-fee/">연수비용안내</a></li>
+                      <li><a href="http://128.199.136.69:81/dormitory/">기숙사안내</a></li>
+                      <li><a href="http://128.199.136.69:81/miscellaneous-fee/">현지비용</a></li>
+                      <li><a href="http://128.199.136.69:81/ielts-official-test-dates/">IELTS  시험 일정</a></li>
+                      <li><a href="http://128.199.136.69:81/toeic-official-test-dates/">TOEIC 시험 일정</a></li>
+                      <li><a href="http://128.199.136.69:81/philippine-holiday/">필리핀 휴일</a></li>
+                      <li><a href="http://128.199.136.69:81/official-pickup-dates/">정규픽업일</a></li>
+                      <li><a href="http://128.199.136.69:81/refund-policy/">환불규정</a></li>
+                      <li><a href="http://128.199.136.69:81/online-registration/">온라인 신청서</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown-submenu">
+                    <a href="#">토크의 학습방법</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="http://128.199.136.69:81/yangco-esl/">ESL 421, 521, 611</a></li>
+                      <li><a href="http://128.199.136.69:81/elts/">IELTS</a></li>
+                      <li><a href="http://128.199.136.69:81/toiec/">TOEIC</a></li>
+                      <li><a href="http://128.199.136.69:81/power-speaking/">Power Speaking</a></li>
+                      <li><a href="http://128.199.136.69:81/job-ready/">Working Holiday</a></li>
+                      <li><a href="http://128.199.136.69:81/family-esl/">Family ESL</a></li>
+                      <li><a href="http://128.199.136.69:81/online-consultation/">연수상담</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown-submenu">
+                    <a href="#">필리핀어학연수계획</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="http://128.199.136.69:81/toeic-test/">TOEIC 시험</a></li>
+                      <li><a href="http://128.199.136.69:81/ielts-test/">IELTS 시험</a></li>
+                      <li><a href="http://128.199.136.69:81/intensive-speaking/">스피킹 집중</a></li>
+                      <li><a href="http://128.199.136.69:81/short-term-study/">1~3주 단기 어학연수</a></li>
+                      <li><a href="http://128.199.136.69:81/long-term-study/">장기유학</a></li>
+                      <li><a href="http://128.199.136.69:81/best-price/">저렴한 어학연수</a></li>
+                      <li><a href="http://128.199.136.69:81/philippines-and-other-countries/">호주, 캐나다 연계연수</a></li>
+                    </ul>
+                  </li>
+                  <li class="dropdown-submenu">
+                    <a href="#">커뮤니티</a>
+                    <ul class="dropdown-menu">
+                      <li><a href="http://128.199.136.69:81/notice/">공지사항</a></li>
+                      <li><a href="http://128.199.136.69:81/experiences/">연수후기</a></li>
+                      <li><a href="http://128.199.136.69:81/meals/">토크의 식단</a></li>
+                      <li><a href="http://128.199.136.69:81/inside-philippine/">미리보는 필리핀</a></li>
+                      <li><a href="http://128.199.136.69:81/best-students-and-teachers/">명예의 전당</a></li>
+                      <li><a href="http://128.199.136.69:81/travel/">여행</a></li>
+                    </ul>
+                  </li>
                 </ul>
               </div>
               <div class="col-xs-12 col-sm-12 col-md-4">
